abee3b18a4e90e501dbaded0f57fca75e92902ce
Armel Steve <ouetchouaarmel@yahoo.com>
2021-03-24 11:46:11 +0100

    Add project files.

---
+++ Camraod/Controllers/CamrodController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Camraod.Controllers
+{
+    public class CamrodController : Controller
+    {
+        // GET: Camrod
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Accueil()
+        {
+            return View("Accueil");
+        }
+        public ActionResult Login()
+        {
+            return View("Login");
+        }
+        public ActionResult Contact()
+        {
+            return View("Contact");
+        }
+        public ActionResult About()
+        {
+            return View("About");
+        }
+        public ActionResult Inscription()
+        {
+            return View("Inscription");
+        }
+        public ActionResult index()
+        {
+            return View("index");
+        }
+    }
+}
